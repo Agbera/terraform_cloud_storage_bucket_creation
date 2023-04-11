33b284686f9cea7f895c0bd92df0b69901bd8e5b
Agbera <126923646+Agbera@users.noreply.github.com>
2023-04-11 07:56:12 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project     = "avian-amulet-378416"
+  project     = "assignment-project-383205"
   region      = "us-central1"
 }
 
 
 terraform {
   cloud {
-    organization = "sami123123123"
+    organization = "terraform-project1"
 
     workspaces {
       name = "mobility-base-storage"
