a37cdb67e242947709c12b4ceab47e52ef117be1
Agbera <126923646+Agbera@users.noreply.github.com>
2023-04-11 10:12:11 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,15 +1,15 @@
 variable "project_id" {
   description = "The ID of the GCP project."
-  default     = "assignment-project-383205"
+  default     = "my-project-1-383207"
 }
 
 variable "bucket_name" {
   description = "A list of unique names for the GCP storage buckets."
   type        = list(string)
   default     = [
-    "my-test-bucket00234544",
-    "mytestbucket8888",
-    "montrealcollege-927218"
+    "my-test-bucket058454",
+    "mytestbucket83238",
+    "montrealcollege-9097218"
     
 
   ]
